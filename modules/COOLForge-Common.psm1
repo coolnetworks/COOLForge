51e1d29211c99e043b4fdd4078aac0486d726adc
Allen Bolderoff <allen@cool.net.au>
2026-01-12 18:45:14 +1030

    Bump module version to 2026.01.12.12 (422 fix + auto-create tags)

--- modules/COOLForge-Common.psm1
+++ modules/COOLForge-Common.psm1
@@ -12,7 +12,7 @@
     - Device information utilities
 
 .NOTES
-    Version:    2026.01.12.09
+    Version:    2026.01.12.12
     Target:     Level.io RMM
     Location:   {{cf_coolforge_msp_scratch_folder}}\Libraries\COOLForge-Common.psm1
 
